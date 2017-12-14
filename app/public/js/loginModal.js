a181b67b5e038ec245aa5b8a2380f9aa00212a7f
Bowen <bowenbrinegar@Bowens-MacBook-Pro.local>
2017-12-14 16:07:19 -0500

    fixed the hover functions

--- app/public/js/loginModal.js
+++ app/public/js/loginModal.js
@@ -9,10 +9,6 @@ var loginHtml = `
           <button type="submit" class="btn btn-lg btn-block btn-warning">Log In -></button>
         </form>`
 
-
-
-
-
 var registerHtml = `
 				<form id='registerHtml' action="/register" method="post">
           <div class="form-group">
@@ -32,6 +28,11 @@ var registerHtml = `
 $('#formContainer').append(registerHtml)
 $('#registerButton').css({"background": "rgba(0,0,0,.75)", "color": "white"})  
 $('#loginButton').css({"background": "rgb(84, 237, 211)"})
+$("#loginButton").hover(function(){
+	  $(this).addClass('hover');
+	}, function(){
+	  $(this).removeClass('hover');
+	});
 
 var turn = true
 $('.userClick').on("click", function() {
@@ -44,30 +45,32 @@ $('.userClick').on("click", function() {
 	turn = !turn;
 })
 	
-
-
 $('#loginButton').on("click", function() {
+	$('#loginButton').off( "mouseenter mouseleave" );
 	$('#formContainer').empty()
 	$('#formContainer').append(loginHtml)
-	$('#registerButton').css({
-		"background": "rgb(84, 237, 211)"})
-	$('#loginButton').css({
-		"background": "rgba(0,0,0,.75)",
-		"color": "white"}) 
-	$('#loginModal').css({
-		"height": "348px"
-	})
-
+	$('#registerButton').css({"background": "rgb(84, 237, 211)"})
+	$('#loginButton').css({"background": "rgba(0,0,0,.75)", "color": "white"}) 
+	$('#loginModal').css({"height": "348px"})
+	$("#registerButton").hover(function(){
+	  $(this).addClass('hover');
+	}, function(){
+	  $(this).removeClass('hover');
+	});
 })
 
 $('#registerButton').on("click", function() {
+	$('#registerButton').off( "mouseenter mouseleave" );
 	$('#formContainer').empty()
 	$('#formContainer').append(registerHtml)
 	$('#registerButton').css({"background": "rgba(0,0,0,.75)", "color": "white"})  
 	$('#loginButton').css({"background": "rgb(84, 237, 211)"}) 
-	 $('#loginModal').css({
-	 	"height": "500px"
-	 })
+	$('#loginModal').css({"height": "500px"})
+	$("#loginButton").hover(function(){
+	  $(this).addClass('hover');
+	}, function(){
+	  $(this).removeClass('hover');
+	});
 })  
 
 
